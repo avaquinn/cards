69090b5efb99bfbd586db8c63bc75952814c609d
Ava Quinn <78455655+avaquinn@users.noreply.github.com>
2025-01-13 10:32:42 -0800

    Deck added

--- Game.java
+++ Game.java
@@ -2,5 +2,13 @@ public class Game {
     public static void main(String[] args)
     {
         System.out.println("foo");
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        for (int i = 0; i < 5; i++) {
+            Card card = deck.draw();
+            System.out.println("Drew card: " + card);
+        }
+
     }
 }
